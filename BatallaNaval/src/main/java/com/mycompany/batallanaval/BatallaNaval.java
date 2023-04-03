9765ec97c6bfd3376495f7db7b470ee31a18f0e7
Carlos López <carlosalfredolopez96@gmail.com>
2023-04-03 14:14:38 -0600

    Agregue la Batalla Naval

---
+++ BatallaNaval/src/main/java/com/mycompany/batallanaval/BatallaNaval.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.batallanaval;
+
+/**
+ *
+ * @author carlo
+ */
+public class BatallaNaval {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+}
